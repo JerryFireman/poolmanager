f7388aa95da579b34bcbed62caf2431c1e2104a0
jerryfireman <jerry_fireman@strucinfo.com>
2020-05-22 11:20:08 -0400

    tweaked

--- migrations/2_deploy_factories.js
+++ migrations/2_deploy_factories.js
@@ -2,10 +2,10 @@ const BFactory = artifacts.require('BFactory');
 const PoolManager = artifacts.require('PoolManager');
 
 module.exports = function(deployer) {
-  deployer.deploy(BFactory)
+  deployer.deploy(BFactory).then(function() {
+    return deployer.deploy(PoolManager, BFactory.address);
+  });
 };
-
-
 
 
 /*
